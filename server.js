f3b3ff19dd415d5a34d94a335ed8d0dcb35cf1fd
M1HNE41 <mihnea1009@gmail.com>
2025-03-14 15:59:13 +0200

    Added root endpoint for server test

--- server.js
+++ server.js
@@ -50,6 +50,9 @@ app.get("/api/data", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
